7dfb8ab68457f9af49a4d251cb71f37bdba6408d
Matt Cribbs <mcribbs@gmail.com>
2021-12-30 08:34:48 -0500

    Move UART code into seperate files

---
+++ src/peripherals/uart.h
@@ -0,0 +1,57 @@
+#ifndef	_UART_H
+#define	_UART_H
+#include <stddef.h>
+#include <stdint.h>
+
+enum {
+    // The GPIO registers base address.
+    GPIO_BASE = 0x3F200000, // for raspi2 & 3, 0x20200000 for raspi1
+
+    GPPUD = (GPIO_BASE + 0x94),
+    GPPUDCLK0 = (GPIO_BASE + 0x98),
+
+    // The base address for UART.
+    UART0_BASE = 0x3F201000, // for raspi2 & 3, 0x20201000 for raspi1
+
+    UART0_DR     = (UART0_BASE + 0x00),
+    UART0_RSRECR = (UART0_BASE + 0x04),
+    UART0_FR     = (UART0_BASE + 0x18),
+    UART0_ILPR   = (UART0_BASE + 0x20),
+    UART0_IBRD   = (UART0_BASE + 0x24),
+    UART0_FBRD   = (UART0_BASE + 0x28),
+    UART0_LCRH   = (UART0_BASE + 0x2C),
+    UART0_CR     = (UART0_BASE + 0x30),
+    UART0_IFLS   = (UART0_BASE + 0x34),
+    UART0_IMSC   = (UART0_BASE + 0x38),
+    UART0_RIS    = (UART0_BASE + 0x3C),
+    UART0_MIS    = (UART0_BASE + 0x40),
+    UART0_ICR    = (UART0_BASE + 0x44),
+    UART0_DMACR  = (UART0_BASE + 0x48),
+    UART0_ITCR   = (UART0_BASE + 0x80),
+    UART0_ITIP   = (UART0_BASE + 0x84),
+    UART0_ITOP   = (UART0_BASE + 0x88),
+    UART0_TDR    = (UART0_BASE + 0x8C),
+};
+
+static inline void mmio_write(uint64_t reg, uint64_t data) {
+    *(volatile uint64_t*)reg = data;
+}
+
+static inline uint64_t mmio_read(uint64_t reg) {
+    return *(volatile uint64_t*)reg;
+}
+
+// Loop <delay> times in a way that the compiler won't optimize away
+static inline void delay(int32_t count) {
+    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
+            : "=r"(count): [count]"0"(count) : "cc");
+}
+
+void uart_init();
+
+void uart_putc(unsigned char c);
+unsigned char uart_getc();
+
+void uart_puts(const char* str);
+
+#endif  /*_UAART_H */
