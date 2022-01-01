fdc3f2b98b95db2f90548a903d0e79d38caa842b
Matt Cribbs <mcribbs@gmail.com>
2021-12-31 22:34:53 -0500

    Support multiple cores

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,18 +1,38 @@
 #include <stdint.h>
 #include "../common/stdio.h"
+#include "../common/utils.h"
 #include "../peripherals/uart.h"
 
-void kernel_main(uint64_t r0, uint64_t r1, uint64_t atags)
+volatile unsigned int semaphore = 0;
+
+void kernel_do(int id)
 {
-    (void) r0;
-    (void) r1;
-    (void) atags;
+	// Wait for previous CPU to finish printing
+	while(id != semaphore) { }
 
-    uart_init();
-    puts("Hello, kernel World!\r\n");
+	puts("Hello, from processor ");
+	putc(id + '0');
+	puts("\r\n");
 
-    while (1) {
-        putc(getc());
-        putc('\n');
-    }
+	// Tells the next CPU to go
+	semaphore++;
+
+    // Only CPU #0 do the echo
+	if (id == 0) {
+		// Wait for everyone else to finish
+		while(semaphore != 4) { }
+
+		while (1) {
+			putc(getc());
+		}
+	}
+}
+
+void kernel_main(void) {
+	uart_init();
+	kernel_do(0);
+}
+
+void kernel_secondary(int core_id) {
+	kernel_do(core_id);
 }
