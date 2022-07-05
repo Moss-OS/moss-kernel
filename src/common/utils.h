42394ee2e755d8e3644e739925eb91c43f1c7227
Matt Cribbs <mcribbs@williamhill.us>
2022-07-04 22:27:34 -0400

    Update UART code to work with Raspi4

--- src/common/utils.h
+++ src/common/utils.h
@@ -5,6 +5,8 @@
 #ifndef __ASSEMBLER__
 
 extern unsigned int get_core_id ( void );
+extern void put32 ( unsigned long, unsigned int);
+extern unsigned int get32 ( unsigned long );
 extern void delay ( unsigned long );
 
 #endif
--- src/peripherals/uart.c
+++ src/peripherals/uart.c
@@ -1,45 +1,55 @@
 #include "uart.h"
 #include "../common/utils.h"
 
-void uart_init()
+void uart_init (void)
 {
-    mmio_write(UART0_CR, 0x00000000);
+	unsigned int selector;
+	selector = get32(GPFSEL1);
+	selector &= ~(7<<12);                   // clean gpio14
+	selector |= 4<<12;                      // set alt0 for gpio14
+	selector &= ~(7<<15);                   // clean gpio15
+	selector |= 4<<15;                      // set alt0 for gpio 15
+	put32(GPFSEL1, selector);
 
-    mmio_write(GPPUD, 0x00000000);
-    delay(150);
+	put32(GPPUD, 0);
+	delay(150);
+	put32(GPPUDCLK0, (1<<14) | (1<<15));
+	delay(150);
+	put32(GPPUDCLK0, 0);
 
-    mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
-    delay(150);
-
-    mmio_write(GPPUDCLK0, 0x00000000);
-
-    mmio_write(UART0_ICR, 0x7FF);
-
-    mmio_write(UART0_IBRD, 1);
-    mmio_write(UART0_FBRD, 40);
-
-    mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
-
-    mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
-            (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
-
-    mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+	put32(UART0_CR, 0);                    // disable UART until configuration is done
+	/* baud divisor = UARTCLK / (16 * baud_rate) = 48 * 10^6 / (16 * 115200) = 26.0416666667
+     * integer part = 26
+	 * fractional part = (int) ((0.0416666667 * 64) + 0.5) = 3
+	 * generated baud rate divisor = 26 + (3 / 64) = 26.046875
+     * generated baud rate = (48 * 10^6) / (16 * 26.046875) = 115177
+	 * error = |(115177 - 115200) / 115200 * 100| = 0.02%
+	 */
+	put32(UART0_IBRD, 26);                 // baud rate divisor, integer part
+	put32(UART0_FBRD, 3);                  // baud rate divisor, fractional part
+	put32(UART0_LCRH, (1<<4) | (3<<5));    // enable FIFOs and 8 bits frames
+	put32(UART0_IMSC, 0);                  // mask interupts
+	put32(UART0_CR, 1 | (1<<8) | (1<<9));  // enable UART, receive and transmit
 }
 
-void uart_putc(unsigned char c)
+void uart_send (char c)
 {
-    while ( mmio_read(UART0_FR) & (1 << 5) ) { }
-    mmio_write(UART0_DR, c);
+	// wait for transmit FIFO to have an available slot
+	while(get32(UART0_FR) & (1<<5)) { }
+	put32(UART0_DR, c);
 }
 
-unsigned char uart_getc()
+char uart_recv ()
 {
-    while ( mmio_read(UART0_FR) & (1 << 4) ) { }
-    return mmio_read(UART0_DR);
+	// wait for receive FIFO to have data to read
+	while(get32(UART0_FR) & (1<<4)) { }
+	return(get32(UART0_DR) & 0xFF);
 }
 
-void uart_puts(const char* str)
+void uart_send_string(char* str)
 {
-    for (size_t i = 0; str[i] != '\0'; i ++)
-        uart_putc((unsigned char)str[i]);
+	for (int i = 0; str[i] != '\0'; i ++) {
+		uart_send((char) str[i]);
+	}
 }
+
