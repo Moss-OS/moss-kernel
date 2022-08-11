bfafa40f4d7c583d25c526f0db29d409af2c356f
Matt Cribbs <mcribbs@gmail.com>
2022-08-11 14:55:20 -0400

    Detect at runtime
    We can detect the raspi version at runtime and set the needed values during UART init instead of at compile time so let's do that!

--- src/common/utils.h
+++ src/common/utils.h
@@ -4,11 +4,19 @@
 
 #ifndef __ASSEMBLER__
 
+extern unsigned int get_core_info ( void );
 extern unsigned int get_core_id ( void );
 extern void put32 ( unsigned long, unsigned int);
 extern unsigned int get32 ( unsigned long );
 extern void delay ( unsigned long );
 
 #endif
+
+enum {
+	RASPI3 = 3,
+	RASPI4 = 4,
+};
+
+int get_raspi_ver ( void );
 
 #endif  /*_UTILS_H */
