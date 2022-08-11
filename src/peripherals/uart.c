c6c990eb62f1353df9f1084d4acdb6cb6514c1d2
Matt Cribbs <mcribbs@gmail.com>
2022-08-11 14:55:20 -0400

    Detect at runtime
    We can detect the raspi version at runtime and set the needed values during UART init instead of at compile time so let's do that!

--- src/peripherals/uart.c
+++ src/peripherals/uart.c
@@ -3,6 +3,48 @@
 
 void uart_init (void)
 {
+	switch (get_raspi_ver()) {
+		case RASPI3:
+			GPIO_BASE = 0x3f200000;   // raspi2 & 3
+			break;
+
+		case RASPI4:
+			GPIO_BASE = 0xFE200000;   // raspi4
+			break;
+
+		default:
+			return;
+			// die?
+	}
+
+	// GPIO
+	GPFSEL1		= (GPIO_BASE + 0x04);
+	GPSET0		= (GPIO_BASE + 0x1C);
+	GPCLR0		= (GPIO_BASE + 0x28);
+	GPPUD		= (GPIO_BASE + 0x94);
+	GPPUDCLK0	= (GPIO_BASE + 0x98);
+
+	// The base address for UART.
+	UART0_BASE   = (GPIO_BASE + 0x1000);
+	UART0_DR     = (UART0_BASE + 0x00);
+	UART0_RSRECR = (UART0_BASE + 0x04);
+	UART0_FR     = (UART0_BASE + 0x18);
+    UART0_ILPR   = (UART0_BASE + 0x20);
+    UART0_IBRD   = (UART0_BASE + 0x24);
+    UART0_FBRD   = (UART0_BASE + 0x28);
+    UART0_LCRH   = (UART0_BASE + 0x2C);
+    UART0_CR     = (UART0_BASE + 0x30);
+    UART0_IFLS   = (UART0_BASE + 0x34);
+    UART0_IMSC   = (UART0_BASE + 0x38);
+    UART0_RIS    = (UART0_BASE + 0x3C);
+    UART0_MIS    = (UART0_BASE + 0x40),
+    UART0_ICR    = (UART0_BASE + 0x44);
+    UART0_DMACR  = (UART0_BASE + 0x48);
+    UART0_ITCR   = (UART0_BASE + 0x80);
+    UART0_ITIP   = (UART0_BASE + 0x84);
+    UART0_ITOP   = (UART0_BASE + 0x88);
+    UART0_TDR    = (UART0_BASE + 0x8C);
+
 	unsigned int selector;
 	selector = get32(GPFSEL1);
 	selector &= ~(7<<12);                   // clean gpio14
@@ -52,4 +94,3 @@ void uart_send_string(const char* str)
 		uart_send((char) str[i]);
 	}
 }
-
