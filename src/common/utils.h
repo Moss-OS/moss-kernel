158f20580adae93dca16103ddb63cacbedf57a4c
Matt Cribbs <mcribbs@gmail.com>
2022-08-14 18:08:03 -0400

    Add function get_el() to get current exception level

--- src/common/utils.h
+++ src/common/utils.h
@@ -6,8 +6,11 @@
 
 extern unsigned int get_core_info ( void );
 extern unsigned int get_core_id ( void );
+extern unsigned int get_el ( void );
+
 extern void put32 ( unsigned long, unsigned int);
 extern unsigned int get32 ( unsigned long );
+
 extern void delay ( unsigned long );
 
 #endif
